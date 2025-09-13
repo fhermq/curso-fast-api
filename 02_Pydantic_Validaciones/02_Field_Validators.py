2665e817d5b49ce30ab0b6ef89d150d450886c81
Fernando Mirasol <fhermq@gmail.com>
2025-09-12 18:11:07 -0600

    Se agrega 02 Field Validators Annotated

--- 02_Pydantic_Validaciones/02_Field_Validators.py
+++ 02_Pydantic_Validaciones/02_Field_Validators.py
@@ -6,10 +6,11 @@ from pydantic import AfterValidator, BaseModel
 # Plain: similar a before, termina la validacion al retornar el valor.
 # Wrap: Flexible para ejecutarse antes o despues de las validaciones de pydantic.
 
-
+## ==========  ANNOTATED =========== ##
 def is_odd (value: int) -> int:
     if value % 2 == 1:
         raise ValueError(f"{value} no es un numero par")
+    return value
     
 NumeroPar = Annotated[int, AfterValidator(is_odd)]
 
@@ -17,5 +18,16 @@ class Model1(BaseModel):
     # NumeroPar = Annotated[int, AfterValidator(is_odd)]
     my_number: NumeroPar
 
-ejemplo: Model1 = Model1(my_number=3)
+#ejemplo: Model1 = Model1(my_number=4)
 
+
+class Model2(BaseModel):
+    other_number: Annotated[NumeroPar, AfterValidator(lambda v : v + 2 )]
+
+#ejemplo2: Model2 = Model2(other_number=0)
+#print(ejemplo2)
+
+class Model3(BaseModel):
+    lista_pares: list[NumeroPar]
+ejemplo3: Model3 = Model3(lista_pares=[-100,6,10])
+print(ejemplo3)
