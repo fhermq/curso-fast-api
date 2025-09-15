decd08ccf270cb03af2a6c02d0a0098c9a750d94
Fernando Mirasol <fhermq@gmail.com>
2025-09-15 10:21:36 -0600

    Se agrega nueva carpeta 03 FastAPI Validations

--- 03_FastAPI_Validaciones/01_Query_Validations.py
+++ 03_FastAPI_Validaciones/01_Query_Validations.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI, Query
 from typing import Annotated
+from pydantic import AfterValidator
 
 app = FastAPI()
+# Strings
+# max_length
+# min_length
+# pattern
+
+# Numbers
+# gt 
+# ge
+# lt
+# le
+
+def check_valid_number(id: str):
+    if id % 2 != 0:
+        raise ValueError("Debe ser un número par")
+    return id
+
 
 @app.get("/items/")
-async def read_items(q: Annotated[ list[str] | None , Query()] = None):
+async def read_items(q: Annotated[ int | None , Query(lt=10, ge=5)] = None):
+#async def read_items(q: Annotated[ int | None , AfterValidator(check_valid_number)] = None):
     results: dict = {"mensaje": "Acceso a get(read_items)"}
     if q:
         results.update({"q": q})
